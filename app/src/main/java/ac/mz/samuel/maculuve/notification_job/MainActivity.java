93cca76bd291c570f40f801a0782b7fac8b3783f
Samuel Maculuve <32066945+SamuelMaculuve@users.noreply.github.com>
2020-05-29 19:10:04 +0200

    Project

---
+++ app/src/main/java/ac/mz/samuel/maculuve/notification_job/MainActivity.java
@@ -0,0 +1,14 @@
+package ac.mz.samuel.maculuve.notification_job;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
